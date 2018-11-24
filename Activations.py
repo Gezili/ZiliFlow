fd528bd261c9f8a456d211bfb16780a28636b07f
Gezili <zilig1998@gmail.com>
2018-11-24 14:26:40 -0500

    Implement Inference step
    Bug fixes, as well as skeletal code

--- Activations.py
+++ Activations.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+class util:
+    
+    def MakeOneHot(arr):
+        pass
+        
+
 class Activation:
     
     '''
@@ -14,22 +20,27 @@ class CostFunc:
         
         #For numerical stability
         
-        max_arr = np.max(arr)
+        max_arr = np.expand_dims(np.max(arr, axis = 1), axis = 1)
         arr = np.exp(arr - max_arr)
-        sum_arr = np.sum(arr)
+        sum_arr = np.expand_dims(np.sum(arr, axis = 1), axis = 1)
         return arr/sum_arr
-        
-        #TODO support batch input
 
-        
+class Optimizer:
+    
+    def SGD(arr):
+        pass
 
 class Layer:
     
-    def __init__(self, Neurons, Activation):
+    def __init__(self, Neurons, Activation, Type):
+        
+        assert Type is 'Dense' or 'Output'
         
         self.Activation = Activation
         self.Neurons = np.zeros(Neurons)
         self.NeuronCount = Neurons
+        self.Type = Type
+        self.Output = np.zeros(Neurons)
         
     '''
     Default initialization from 
@@ -49,20 +60,28 @@ class Layer:
                 -bound, bound,
                 [1, self.NeuronCount]
             )
+            
+class Optimizer:
+    
+    def SGD(arr):
+        pass
+        
         
 class Graph:
     
-    def __init__(self, InputDim, OutputDim, *args):
+    def __init__(self, InputDim, Output, Optimizer = Optimizer.SGD, *args):
+        
+        self.Output = Output
         
         self.InputDim = InputDim
-        self.OutputDim = OutputDim
+        self.OutputDim = Output.NeuronCount
         
         self.Input = np.zeros(InputDim)
-        self.Output = np.zeros(OutputDim)
+        
         self.Graph = [self.Input]
         for i, arg in enumerate(args):
             #We need to make sure that the arguments are unique, otherwise Python will 
-            #append too many 
+            #point to the same object in multiple locations on the list
             self.Graph.append(arg)
             if i >= 1:
                 assert arg not in args[0:i]
@@ -74,14 +93,30 @@ class Graph:
     def WeightInitializer(self):
 
         self.Graph[1].InitializeWeights(self.InputDim) 
-        for i in range(2, len(self.Graph) - 1):
+        for i in range(2, len(self.Graph)):
             
             neurons_prev = self.Graph[i - 1].NeuronCount
             self.Graph[i].InitializeWeights(neurons_prev)
+            
+    def RunInferenceStep(self, arr):
+        
+        self.Graph[1].Output = Activation.ReLU(np.dot(self.Graph[1].Weights, arr) +\
+        self.Graph[1].Biases)
+        
+        for i in range(2, len(self.Graph)):
+            self.Graph[i].Output = self.Graph[i].Activation(np.dot(self.Graph[i].Weights,\
+            self.Graph[i - 1].Output[0]) + self.Graph[i].Biases)
+            
+        return self.Graph[len(self.Graph) - 1].Output
+        
+    def RunBackpropStep(self, arr, label):
+        
+        
         
 if __name__ == '__main__':
     
-    l = Layer(12, Activation.ReLU)
-    t = Layer(12, Activation.ReLU)
-    p = Layer(12, Activation.ReLU)
-    g = Graph(5, 5, l, t)
+    a = Layer(12, Activation.ReLU, 'Dense')
+    b = Layer(12, Activation.ReLU, 'Dense')
+    c = Layer(12, Activation.ReLU, 'Dense')
+    output = Layer(5, CostFunc.Softmax, 'Output')
+    g = Graph(5, output, a, b, c)
